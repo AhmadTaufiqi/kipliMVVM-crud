6503cb756ad1db20145d78b0570a6e30c1580ada
AhmadTaufiqi <62783677+AhmadTaufiqi@users.noreply.github.com>
2020-05-21 05:48:41 +0700

    training1

---
+++ app/src/main/java/com/kipli/training/MainActivity.java
@@ -0,0 +1,16 @@
+package com.kipli.training;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kipli.training.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
